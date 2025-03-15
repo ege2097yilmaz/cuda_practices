7131636fc21925455983781240261e49ddb737e6
Ege Yılmaz <egeyilmaz.otonom@gmail.com>
2025-03-15 18:13:08 +0300

    pipe start and finish positions

--- Physics_simulations/flow_simulation/include/fluid_simulation.h
+++ Physics_simulations/flow_simulation/include/fluid_simulation.h
@@ -10,6 +10,9 @@
 #define BLOCK_SIZE 16
 #define TIME_STEP 0.01f
 
+#define PIPE_START (WIDTH / 4)  
+#define PIPE_END (3 * WIDTH / 4)
+
 extern float *h_velocityX, *h_velocityY, *h_pressure;
 extern float *d_velocityX, *d_velocityY, *d_pressure;
 
@@ -17,6 +20,9 @@ void init_simulation();
 void step_simulation();
 
 __global__ void update_velocity(float *velocityX, float *velocityY, float *pressure);
-__global__ void apply_boundaries(float *velocityX, float *velocityY);
+__global__ void update_velocity_kernel(float *velocityX, float *velocityY, float *pressure);
+__global__ void pressure_solver_kernel(float *pressure, float *velocityX, float *velocityY);
+__global__ void apply_boundary_conditions_kernel(float *velocityX, float *velocityY);
+
 
 #endif
